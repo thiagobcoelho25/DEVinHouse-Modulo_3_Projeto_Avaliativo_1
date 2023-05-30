c6f9dc5cbe7de8021129863d9bcb228821843d12
Thiago Ribeiro <thiagobcoelho25@gmail.com>
2023-05-30 11:27:12 -0300

    feat: adição carga inicial de dados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,20 @@
 INSERT INTO passageiro (id, cpf, nome, data_nascimento, classificacao, milhas)
-    VALUES (1, '12922887337', 'Thiago', '2023-05-29', 'VIP', 100);
+    VALUES (1, '000.000.000-00', 'Rachel Green', '1969-01-11', 'VIP', 100);
 INSERT INTO passageiro (id, cpf, nome, data_nascimento, classificacao, milhas)
-    VALUES (2, '12345678989', 'Guto', '2023-05-29', 'OURO', 80);
-
-
-
--- INSERT INTO passageiro (id, cpf, nome, data_nascimento, classificacao, milhas)
---     VALUES (RANDOM_UUID(), '12922887337', 'Thiago', '2023-05-29', 'VIP', 200);
+    VALUES (2, '111.111.111-11', 'Phoebe Buffay', '1963-07-30', 'OURO', 100);
+INSERT INTO passageiro (id, cpf, nome, data_nascimento, classificacao, milhas)
+    VALUES (3, '222.222.222-22', 'Ross Geller', '1966-11-02', 'PRATA', 100);
+INSERT INTO passageiro (id, cpf, nome, data_nascimento, classificacao, milhas)
+    VALUES (4, '333.333.333-33', 'Monica Geller', '1964-06-15', 'OURO', 100);
+INSERT INTO passageiro (id, cpf, nome, data_nascimento, classificacao, milhas)
+    VALUES (5, '444.444.444-44', 'Chandler Bing', '1969-08-19', 'OURO', 100);
+INSERT INTO passageiro (id, cpf, nome, data_nascimento, classificacao, milhas)
+    VALUES (6, '555.555.555-55', 'Joey Tribbiani', '1967-07-25', 'BRONZE', 100);
+INSERT INTO passageiro (id, cpf, nome, data_nascimento, classificacao, milhas)
+    VALUES (7, '666.666.666-66', 'Mike Hannigan', '1969-04-06', 'VIP', 100);
+INSERT INTO passageiro (id, cpf, nome, data_nascimento, classificacao, milhas)
+    VALUES (8, '777.777.777-77', 'Gunther Tyler', '1962-05-28', 'ASSOCIADO', 100);
+INSERT INTO passageiro (id, cpf, nome, data_nascimento, classificacao, milhas)
+    VALUES (9, '888.888.888-88', 'Janice Wheeler', '1961-08-07', 'BRONZE', 75);
+INSERT INTO passageiro (id, cpf, nome, data_nascimento, classificacao, milhas)
+    VALUES (10, '999.999.999-99', 'Richard Burke', '1945-01-29', 'BRONZE', 50);
